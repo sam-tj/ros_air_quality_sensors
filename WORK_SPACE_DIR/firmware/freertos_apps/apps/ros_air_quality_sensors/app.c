92bb85dcc23a2772cfdb2d7ad9c81393cc76dc8e
Sameer Tuteja <sameer.tuteja@alumni.fh-aachen.de>
2022-06-05 11:13:55 +0200

    sensor topic fix

--- WORK_SPACE_DIR/firmware/freertos_apps/apps/ros_air_quality_sensors/app.c
+++ WORK_SPACE_DIR/firmware/freertos_apps/apps/ros_air_quality_sensors/app.c
@@ -131,7 +131,7 @@ void appMain(void *arg)
 	RCCHECK(rclc_publisher_init_default(
 		&publisher,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Sensdata),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(custom_message, msg, Sensdata),
 		"sensors"));
 
 	// create timer,
